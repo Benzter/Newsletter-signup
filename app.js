82bea28ce6fff0c20a53a563710d782b676e89dd
Benzter <Dhanushkanuwanpremasiri28@gmail.com>
2022-12-27 21:18:38 +0530

    added mailchimp api to collect subscriber details

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
+const { response } = require('express');
 
 const app = express();
 app.use(express.static("public"));
@@ -13,12 +15,44 @@ app.get("/",function(req,res){
 })
 
 app.post("/",function(req,res){
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var email = req.body.email;
-    console.log(firstName , lastName , email);
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    
+    const data = {
+        members:[{
+            email_address:email,
+            status:"subscribed",
+            merge_fields:{
+                FNAME:firstName,
+                LNAME:lastName}}
+            ]
+    }
+
+    const jsondata = JSON.stringify(data);
+   
+   
+    const url = "https://us21.api.mailchimp.com/3.0/lists/112eeb3a20"
+    const options = {
+        method:"POST",
+        auth:"Benzter:790380658db5b5b8e819f1ae07b3a744-us21"
+    }
+
+    const request = https.request(url,options,function(response){
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsondata);
+    request.end();
 })
 
 app.listen(3000,function(){
     console.log("server is succesfully started on port 3000")
 })
+
+
+
+// 790380658db5b5b8e819f1ae07b3a744-us21
+// 112eeb3a20
